88dc637c8372c0d171fb501851fcb012fe314a54
Andrengr0 <andrenegreirosmoreira@gmail.com>
2022-09-28 17:56:11 -0300

    criando funções

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,37 @@
 $(()=>{
 
+    var qtdNumeros = $('#qtd-numeros-megasena')
+    var allNumbers = $('#allNumbers')
+    var numImpares = $('#numImpares')
+    var numPares = $('#numPares')
+    
+
+    for(var i=6; i<=15; i++)(
+        qtdNumeros.append('<option value="+ item +">' + i + '</option>')
+    )
+
+    ativaRadioInicial();
+    function ativaRadioInicial(){
+        $("input[value=allNumbers]").prop("checked", true);
+    }
+
+
+    $('.btn-gerar-result > #gerar-resultado').on('click', function() {
+        idInput = $('input[name=option]:checked').val();
+        
+        if(idInput == 'allNumbers'){
+            console.log(getRandomArbitrary(10, 21));
+        }else if(idInput == 'numImpares'){
+            console.log('impares')
+        }else if(idInput == 'numPares'){
+            console.log('pares')
+        }
+    });
+    
+
+    function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+      }
 
     
 })

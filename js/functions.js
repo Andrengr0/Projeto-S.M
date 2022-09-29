d0e8bf633656260a9286fb0de445061dcc05c326
Andrengr0 <andrenegreirosmoreira@gmail.com>
2022-09-29 01:13:09 -0300

    adicionando funções

--- js/functions.js
+++ js/functions.js
@@ -16,13 +16,50 @@ $(()=>{
     }
 
 
+    function verificaImpar(num){
+        var testaNum = num % 2;
+        if(testaNum != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
     $('.btn-gerar-result > #gerar-resultado').on('click', function() {
+        for(var i=1; i<=60; i++){
+            $('#'+i).css('background-color','#1B9C20')
+        }
+
         idInput = $('input[name=option]:checked').val();
+        var qtdNumerosParaGerar = parseInt($('select#qtd-numeros-megasena option:checked').text());
         
         if(idInput == 'allNumbers'){
-            console.log(getRandomArbitrary(10, 21));
+            
+            var numerosSorteados = [];
+
+            var i=0;
+            while( i<qtdNumerosParaGerar){
+                var numGerado = getRandomArbitrary(1, 61);
+                if(numerosSorteados.includes(numGerado) == false){
+                    numerosSorteados.push(numGerado)
+                    i++;
+                }
+            }
+  
+            for(var i=0; i<numerosSorteados.length; i++){
+                var seletorParaID = numerosSorteados[i];
+                $('#'+seletorParaID).css('background-color','#560BAD')
+            }
+
+
         }else if(idInput == 'numImpares'){
-            console.log('impares')
+
+
+            if(verificaImpar(qtdNumerosParaGerar) == true){
+                console.log('num impar');
+            }
+
         }else if(idInput == 'numPares'){
             console.log('pares')
         }
